f3dd19d6e887922736d065099e7725e1446565cf
hanhj <mmhjh@sohu.com>
2016-06-09 07:20:48 +0800

    add test.c

---
+++ test.c
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: hanhj
+	> Mail: mmhjh@sohu.com 
+	> Created Time: 2016年06月09日 星期四 07时18分52秒
+ ************************************************************************/
+
+#include<stdio.h>
+void main(int argc,char **argv){
+
+	printf("Hello World\n");
+}
