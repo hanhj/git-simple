3e09bef63dd19ab4aba8cfae1c90ca6a5222379a
hanhj <mmhjh@sohu.com>
2019-07-15 21:02:23 +0800

    tmp

--- data_stru/graph.h
+++ data_stru/graph.h
@@ -13,9 +13,55 @@ template <class T>
 class MGraph{
 	public:
 		int edge[MaxPoint][MaxPoint];
-		T data[MaxPoint];
-		int n,e;//point and edges
+		T vec[MaxPoint];
+		int n,e;//points and edges
+
+		MGraph(){
+			n=0;
+			e=0;
+			int i;
+			for(i=0;i<MaxPoint;i++)
+				vistor[i]=0;
+		}
+		void visit(int);
+		int vistor[MaxPoint];
+		void dfs(){
+
+		}
+		void bfs(){
+
+		}
+
 };
+class AdjEdge{
+	public:
+		int v;
+		AdjEdge *next;
+};
+template <class T>
+class AdjNode{
+	public:
+		T data;
+		AdjEdge*first;
+};
+template <class T>
+class AdjGraph{
+	public:
+		void visit(AdjNode *);
+		AdjNode vec[MaxPoint];
+		AdjGraph(){
+			int i;
+			for(i=0;i<MaxPoint;i++){
+				vec[i].first=NULL;
+			}
+		}
+		void dfs(){
+
+		}
+		void bfs(){
+
+		}
+}
 void test_graph();
 #endif //__graph_h
 // vim:tw=72 
