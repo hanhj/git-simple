38f470ccb9ad1b6a914f3fbab0aaeb837ebc0f4a
hanhj <hanhj@zx-jy.com>
2019-07-15 19:42:33 +0800

    graph

--- data_stru/graph.h
+++ data_stru/graph.h
@@ -8,14 +8,230 @@
 #define __graph_h
 #include "data_type.h"
 #include "common.h"
+#include <queue>
+#include <vector>
 #define MaxPoint 100
+#define INFI 9999
 template <class T>
 class MGraph{
 	public:
-		int edge[MaxPoint][MaxPoint];
-		T data[MaxPoint];
-		int n,e;//point and edges
+		int **edge;
+		int *visitor;
+		T *vec;
+		int cur_n;
+		int n,e;//points and edges
+		MGraph(){
+			n=0;
+			e=0;
+			cur_n=0;
+			vec=NULL;
+			edge=NULL;
+		}
+		MGraph(int size){
+			n=size;
+			cur_n=0;
+			e=0;
+			vec=new T[size];
+			visitor=new int[size];
+			edge=new int *[size];
+			for(int i=0;i<size;i++){
+				edge[i]=new int [size];
+				visitor[i]=0;
+				for(int j=0;j<size;j++)
+					edge[i][j]=INFI;
+			}
+		}
+		~MGraph(){
+			if(vec)
+				delete []vec;
+			vec=NULL;
+			if(edge){
+				for(int i=0;i<n;i++){
+					if(edge[i])
+						delete []edge[i];
+				}
+				delete []edge;
+			}
+			n=0;
+			e=0;
+			if(visitor)
+				delete []visitor;
+		}
+		void visit(int k){
+			if(k>n-1||k<0)
+				return;
+			visitor[k]=1;
+			cout<<vec[k]<<",";
+		}
+		void clear_visitor(){
+			int i;
+			for(i=0;i<n;i++)
+				visitor[i]=0;
+		}
+		void insert_element(T da){
+			if(cur_n>=n)
+				return ;
+			vec[cur_n++]=da;
+		}
+		void insert_edge(int v,int w,int weight){
+			if(v>n-1||v<0)
+				return ;
+			if(w>n-1||w<0)
+				return ;
+			edge[v][w]=weight;
+			e++;
+		}
+		void dfs(){
+
+		}
+		void bfs(){
+
+		}
 };
+template <class T>
+ostream & operator <<(ostream &os,MGraph<T>&g ){
+	int i;
+	int j;
+	for(i=0;i<g.n;i++){
+		os<<g.vec[i]<<":";
+		for(j=0;j<g.n;j++){
+			if(g.edge[i][j]!=INFI)
+				os<<g.vec[j]<<"("<<g.edge[i][j]<<")"<<",";
+		}
+		os<<endl;
+	}
+	return os;
+}
+class EdgeNode{
+	public:
+		int no;
+		int weight;
+		EdgeNode *next;
+};
+template <class T>
+class AdjNode{
+	public:
+		T data;
+		int no;
+		int vis;
+		EdgeNode *first;
+};
+template <class T>
+class AdjGraph{
+	public:
+		vector<AdjNode<T> *> vec;
+		int n,e;
+		int nn;
+		AdjGraph(){
+			n=0;
+			e=0;
+			nn=0;
+		}
+		~AdjGraph(){
+			int i;
+			EdgeNode *p,*q;
+			for(i=0;i<n;i++){
+				p=vec[i]->first;
+				while(p){
+					q=p;
+					p=p->next;
+					delete q;
+				}
+				delete vec[i];
+			}
+			vec.clear();
+			n=0;
+			e=0;
+		}
+		void insert_element(T da){
+			AdjNode<T> *p;
+			p=new AdjNode<T>;
+			p->data=da;
+			p->first=NULL;
+			p->vis=0;
+			p->no=n;
+			vec.push_back(p);
+			n++;
+		}
+		void insert_edge(int v,int w,int weight){
+			if(v>n-1||v<0)
+				return ;
+			if(w>n-1||w<0)
+				return ;
+			EdgeNode *p,*q;
+			p=new EdgeNode;
+			p->no=w;
+			p->weight=weight;
+			p->next=NULL;
+			q=vec[v]->first;
+			if(q==NULL){
+				vec[v]->first=p;
+				return ;
+			}
+			while(q->next!=NULL){
+				q=q->next;
+			}
+			q->next=p;
+		}
+		void visit(AdjNode<T> *root){
+			root->vis=1;
+			cout<<root->data<<",";
+		}
+		void clear_visor(){
+			int i;
+			for(i=0;i<n;i++)
+				vec[i]->vis=0;
+		}
+		EdgeNode * firstAdj(AdjNode<T> *root){
+			if(root->first==NULL)
+				return NULL;
+			return root->first;
+		}
+		EdgeNode * nextAdj(EdgeNode *p){
+			if(p==NULL)
+				return NULL;
+			else
+				return p->next;
+		}
+		void _dfs(AdjNode<T> *root){
+			EdgeNode *p;
+			if(root==NULL)
+				return;
+			visit(root);
+			p=firstAdj(root);
+			if(p!=NULL){
+				if(vec[p->no]->vis==0)
+					_dfs(vec[p->no]);
+			}
+			p=nextAdj(p);
+			while(p){
+				_dfs(vec[p->no]);
+				p=nextAdj(p);
+			}
+		}
+		void dfs(){
+			_dfs(vec[0]);
+		}
+		void bfs(){
+
+		}
+};
+
+template <class T>
+ostream & operator <<(ostream &os,AdjGraph<T>&g ){
+	int i;
+	EdgeNode *p;
+	for(i=0;i<g.n;i++){
+		os<<g.vec[i]->data<<":";
+		p=g.vec[i]->first;
+		while(p){
+			os<<g.vec[p->no]->data<<"("<<p->weight<<")"<<",";
+			p=p->next;
+		}
+		os<<endl;
+	}
+	return os;
+}
 void test_graph();
 #endif //__graph_h
 // vim:tw=72 
