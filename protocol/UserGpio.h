4343d96b338a9a83a05892ec4131a2718c50d8a1
hanhj <hanhj@zx-jy.com>
2019-10-23 17:15:33 +0800

    protocol

---
+++ protocol/UserGpio.h
@@ -0,0 +1,22 @@
+/*
+ * File Name: UserGpio.h
+ * Author: hanhj
+ * Mail: hanhj@zx-jy.com 
+ * Created Time: 2019-10-23 16:39:23 week:3
+*/
+#ifndef __UserGpio_h
+#define __UserGpio_h
+ 
+#define YX1 1
+#define YX2 1
+#define YX3 1
+#define YX4 1
+#define YX5 1
+#define YX6 1
+#define YX7 1
+#define YX8 1
+#define YX9 1
+#define YX10 10
+
+#endif //__UserGpio_h
+// vim:tw=72 
---
+++ protocol/para.h
@@ -0,0 +1,13 @@
+/*
+ * File Name: para.h
+ * Author: hanhj
+ * Mail: hanhj@zx-jy.com 
+ * Created Time: 2019-10-23 16:15:19 week:3
+*/
+#ifndef __para_h
+#define __para_h
+#define S_YX 1
+#define D_YX 2
+ 
+#endif //__para_h
+// vim:tw=72 
