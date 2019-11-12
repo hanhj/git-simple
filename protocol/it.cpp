8122aa7a5c21d235ac8af0e716abebe31be79b8c
hanhj <hanhj@zx-jy.com>
2019-11-12 10:55:49 +0800

    mv t2 to it.cpp

--- protocol/it.cpp
+++ protocol/it.cpp
@@ -9,12 +9,42 @@
 using namespace std;
 #include <list>
 #include <vector>
+
+/*如何使用迭代器：
+ * 1、使用迭代器需要的类：
+ * 节点类，用于封装数据。
+ * 容器类，用于封装容器,容纳数据。
+ * 迭代类，用于封装指针。用于遍历容器类中的节点数据。
+ * 2.各个类中的要求
+ * 2.1节点类没有什么特殊要求。
+ * 2.2迭代类，
+ *	定义一个节点指针用于记录当前数据位置。
+ *	定义构造函数，赋值节点指针。
+ *	重载* 解 操作函数，返回值为节点的数据引用。
+ *	重载前++，后++操作符函数，
+ *		返回值是迭代类的应用或数。
+ *		前者返回引用，因为改变了数据指针位置，或者数。前++：本身变，返回变
+ *		后者返回一个数。后++：本身变，返回不变。
+ *	->指向函数
+ *		返回节点指针。
+ *	均是单目运算符。
+ *2.3容器类
+ *	用typedef _list_iterator<T> iterator
+ *	定义一个迭代类的名，方便使用。符合stl通用做法。
+ *	定义begin，end函数，用于赋值迭代类实例。返回值是迭代类实例。
+ *
+ * */
 template <typename T>
 class _node{
 	public:
 		T data;
 		_node<T> *next;
+		_node(const T& da){
+			data=da;
+			next=NULL;
+		}
 };
+
 template<typename T>
 class _list_iterator{
 	public:
@@ -23,24 +53,24 @@ class _list_iterator{
 	typedef typename T::value_type value_type;
 	typedef typename T::pointer pointer;
 */
+	typedef _node<T> Node;
 	typedef _list_iterator<T> self;
-	_node<T> *node;
+	Node *node;
 	_list_iterator(){
 
 	}
-	_list_iterator(_node<T> *n):node(n){
+	_list_iterator(Node *n):node(n){
 		
 	}
-	T&  operator *(){
-		return (node->data);
+	T& operator *(){
+		return node->data;
 	}
-	T & operator ->(){
-		return *node;
+	Node* operator ->(){
+		return node;
 	}
-
-	self  operator++(){
+	self& operator++(){
 		node=node->next;
-		return (self)node;
+		return (self&)node;
 	}
 	self operator ++(int){
 		_node<T> *tmp;
@@ -48,11 +78,11 @@ class _list_iterator{
 		node=node->next;
 		return (self)tmp;
 	}
-	bool operator !=(_list_iterator<T> *da){
-		return node!=da->node;
+	bool operator !=(const self &da){
+		return node!=da.node;
 	}
-	bool operator ==(_list_iterator<T> *da){
-		return node == da->node;
+	bool operator ==(const self &da){
+		return node == da.node;
 	}
 
 };
@@ -60,7 +90,7 @@ template <typename T>
 class List{
 	public:
 		int size;
-		typedef _list_iterator<T>*  iterator;
+		typedef _list_iterator<T>  iterator;
 		_node<T> *node;
 
 		_node<T> *head;
@@ -106,7 +136,7 @@ class List{
 			return iterator(head->next);
 		}
 		iterator end(){
-			return iterator(tail);
+			return iterator(tail->next);
 		}
 		int isEmpty(){
 			return size==0?1:0;
@@ -176,8 +206,19 @@ void myswap(Iter a,Iter b){
 	*a=*b;
 	*b=tmp;
 	cout<<"after:"<<*a<<" "<<*b<<endl;
-
 }
+template <typename T>
+class AA{
+	public:
+		T aa;
+		AA(const T &da):aa(da){
+
+		}
+		T & operator *(){
+			cout<<"aaa:"<<aa<<endl;
+			return aa;
+		}
+};
 int main(){
 	vector<std::string> bb {"1","2"};
 	myswap(begin(bb),begin(bb)+1);
@@ -190,10 +231,14 @@ int main(){
 	list.push_back(a);
 	cout<<list<<endl;
 
-	List<int>::iterator it;
-	it=list.begin();
-	//cout<<*it<<endl;
-	cout<<it->data;
+	List<int>::iterator it=list.begin();
+	List<int>::iterator it2;
+
+	while(it!=list.end()){
+		cout<<*it<<endl;
+		a=it->data;
+		it++;
+	}
 
 	
 	cout<<ll.max_size()<<endl;
