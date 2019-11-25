202fa11091cd33b6b76197969d3507d99a850639
hanhj <hanhj@zx-jy.com>
2019-11-25 18:15:46 +0800

    protocol :add it.h

---
+++ protocol/it.h
@@ -0,0 +1,22 @@
+/*
+ * File Name: it.h
+ * Author: hanhj
+ * Mail: hanhj@zx-jy.com 
+ * Created Time: 2019-11-25 11:33:27 week:1
+*/
+#ifndef __it_h
+#define __it_h
+ 
+class AA{
+	int data;
+	public:
+		AA(int a){
+			data=a;
+		}
+};
+class BB{
+	public:
+		AA aa;
+};
+#endif //__it_h
+// vim:tw=72 
